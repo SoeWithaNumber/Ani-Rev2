ec26d14ba55ffae2394c80616fdc6e2f1eff857e
Tyler <tysisson@comcast.net>
2022-09-24 14:55:54 -0400

    Added string option for character

--- commands/character.js
+++ commands/character.js
@@ -1,13 +1,18 @@
 const {SlashCommandBuilder} = require("discord.js");
 function execute(interaction) {
-    console.log("Character Command")
+    console.log(interaction.options.getString("character"))
 }
 
 module.exports = {
     execute,
     data: new SlashCommandBuilder()
         .setName("character")
-        .setDescription("Searches for a character"),
+        .setDescription("Searches for a character")
+        .addStringOption(option =>
+            option.setName("character")
+            .setDescription("The name of the character you want")
+            .setRequired(true)
+        ),
     name: "character"
 
 }
