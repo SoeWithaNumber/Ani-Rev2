63871b9eab9a501c8e0b20e8171412186f684973
Tyler <tysisson@comcast.net>
2022-09-24 15:48:28 -0400

    Added request and reply Untested

--- commands/character.js
+++ commands/character.js
@@ -1,6 +1,49 @@
 const {SlashCommandBuilder} = require("discord.js");
-function execute(interaction) {
-    console.log(interaction.options.getString("character"))
+const {characterEmbedGenerator} = require("../helper/characterEmbed.js")
+async function execute(interaction) {
+    let characterName = interaction.options.getString("character")
+    let request = {
+        data:JSON.stringify({
+            query: `
+            query($name: String!){
+                Character(search: $name) {
+                    name {
+                        first
+                        middle
+                        last
+                        full
+                        native
+                        userPreferred
+                    }
+                gender
+                age
+                image {
+                    large
+                }
+                dateOfBirth {
+                    year
+                    month
+                    day
+                }
+                description
+                isFavourite
+                }
+            }
+            `,
+            variables:{
+                name: characterName
+            }
+        })
+    }
+
+    let aniUsers = interaction.client.anilistUsers
+    
+    if (aniUsers.has(interaction.user.id)) request.headers = { "Authorization": `Bearer ${aniUsers.get(interaction.user.id)}`}
+    let characterInfo = await interaction.client.axios.request(request).catch(console.log)
+
+    let characterEmbed = characterEmbedGenerator(characterInfo.character)
+    interaction.reply()
+
 }
 
 module.exports = {
