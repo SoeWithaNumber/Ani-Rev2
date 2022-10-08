cd4a4f4ab3de5f0a237741fecd22dfefa48f1499
Tyler <tysisson@comcast.net>
2022-10-08 13:32:57 -0400

    Added comments to the search embed file
    Fixed bugs with search embed
    bugs fixed:
    Fixed bug where some response values were sometimes null.
    Values were volumes, chapters, episodes, and duration.
    
    Fixed bug where tags were not taken into consideration.
    Description now replaces markup tags with Discord markup.
    
    Fixed bug where episodes watched/chapters read was not cast to string.

--- helper/searchEmbed.js
+++ helper/searchEmbed.js
@@ -3,38 +3,53 @@ function searchEmbedGenerator(interaction,mediaInfo){
     const searchEmbed = new EmbedBuilder()
     .setTitle(mediaInfo.title.userPreferred) //stuff that stays the same
     .setColor(mediaInfo.coverImage.color)
-    .setDescription(mediaInfo.description)
     .setThumbnail(mediaInfo.coverImage.large)
     .setImage(mediaInfo.bannerImage)
 
-   if (mediaInfo.type == "ANIME") {
+    //If the media is an anime, add the episodes and episode length
+    if (mediaInfo.type == "ANIME") {
         searchEmbed.addFields(
-            {name:"Episodes", value:mediaInfo.episodes.toString(),inline:true},
-            {name:"Episode Length", value:mediaInfo.duration.toString(),inline:true},
+            {name:"Episodes", value: mediaInfo.episodes ? mediaInfo.episodes.toString() : "Unknown",inline:true},
+            {name:"Episode Length", value: mediaInfo.duration ? mediaInfo.duration.toString() : "Unknown",inline:true},
         
         ) 
-    } else {
+    } else { //Otherwise, it has to be a manga, so add volumes and chapters
         searchEmbed.addFields(
-            {name:"Volumes", value:mediaInfo.volumes.toString(),inline:true},
-            {name:"Chapters", value:mediaInfo.chapters.toString(),inline:true}
+            {name:"Volumes", value: mediaInfo.volumes ? mediaInfo.volumes.toString() : "Unknown",inline:true},
+            {name:"Chapters", value: mediaInfo.chapters ? mediaInfo.chapters.toString() : "Unknown",inline:true}
         )
     }
+    //Processing the text for the watch status and the format of the media
     mediaInfo.status = mediaInfo.status[0]+mediaInfo.status.toLowerCase().substring(1).replaceAll("_"," ")
     mediaInfo.format = mediaInfo.format[0]+mediaInfo.format.toLowerCase().substring(1).replaceAll("_"," ")
+    
+    //Adding the genres, air/publishing status, and format
     searchEmbed.addFields(
-        {name:"Genres", value:mediaInfo.genres.join(", ")},
+        {name:"Genres", value: mediaInfo.genres.length > 0 ? mediaInfo.genres.join(", ") : "None"},
         {name:mediaInfo.type == "ANIME"?"Air Status":"Publishing Status", value:mediaInfo.status,inline:true},
         {name:"Format", value:mediaInfo.format,inline:true}
     )
+
+    //Processing the description and adding it. For some god forsaken reason, even when I use the no html option, it still comes in as html...
+    mediaInfo.description = mediaInfo.description.replaceAll(/<br>/g, "").replaceAll(/<i>|<\/i>/g, "*").replaceAll(/<b>|<\/b>/g, "**")
+    searchEmbed.setDescription(mediaInfo.description)
     
+    //If the user isn't logged in, the media is not private, or the show isn't actually on the list, return early
     if(!interaction.client.anilistUsers.has(interaction.user.id) || mediaInfo?.mediaListEntry?.private || !mediaInfo.mediaListEntry) return searchEmbed
+
+    //Process the text for the watch/read status of the media
     mediaInfo.mediaListEntry.status = mediaInfo.mediaListEntry.status[0]+mediaInfo.mediaListEntry.status.toLowerCase().substring(1).replaceAll("_"," ")
+    
+    //Adding the watch/read status and the episode/chapter count
     searchEmbed.addFields(
         {name:mediaInfo.type == "ANIME"?"Watch Status":"Read Status", value:mediaInfo.mediaListEntry.status},
-        {name:mediaInfo.type == "ANIME"?"Episodes Watched":"Chapters Read", value:mediaInfo.mediaListEntry.progress,inline:true}
+        {name:mediaInfo.type == "ANIME"?"Episodes Watched":"Chapters Read", value:mediaInfo.mediaListEntry.progress.toString(),inline:true}
     )
-    if(mediaInfo.type == "MANGA") searchEmbed.addFields({name:"Volumes Read", value:mediaInfo.mediaListEntry.progressVolumes})
 
+    //If the volumes property exists, add it
+    if(mediaInfo.mediaListEntry.progressVolumes) searchEmbed.addFields({name:"Volumes Read", value:mediaInfo.mediaListEntry.progressVolumes})
+
+    //If the score type is the 3-point smileys, figure out which smiley to add then add it
     if (mediaInfo.mediaListEntry.user.mediaListOptions.scoreFormat == "POINT_3"){
         switch(mediaInfo.mediaListEntry.score){
             case 0: searchEmbed.addFields({name:"Score", value:"Unrated",inline:true}); break
@@ -42,10 +57,14 @@ function searchEmbedGenerator(interaction,mediaInfo){
             case 2: searchEmbed.addFields({name:"Score", value:"😐",inline:true}); break
             case 3: searchEmbed.addFields({name:"Score", value:"☺",inline:true}); break
         }
-    } else {
+    } else { //Otherwise, just use the number
         searchEmbed.addFields({name:"Score", value:mediaInfo.mediaListEntry.score > 0?mediaInfo.mediaListEntry.score.toString():"Unrated",inline:true})
     }
+
+    //Add favorited status
     searchEmbed.addFields({name:"Favorited", value:mediaInfo.isFavourite ?"Yes":"No",inline:true})
+
+    
     return searchEmbed;
 }
 
